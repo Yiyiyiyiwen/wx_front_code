fb72423fb718b55026d8f16f83e212929ff9c24e
胡欣玥 <1695234793@qq.com>
2018-11-27 20:01:50 +0800

    1

---
+++ hxy/pages/shouye/shouye.js
@@ -0,0 +1,109 @@
+// pages/shouye/shouye.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   array:[
+     {
+     pic: "/images/one.jpg",
+     name:"壁纸分享日记",
+     small:"16分钟前",
+    word:"易烊千玺（Jackson），2000年11月28日出生于湖南怀化，中国内地流行乐男歌手、舞者、演员，华语演唱组合TFBOYS成员。 2005年，易烊千玺首登电视荧屏，开始参演各类综艺节目。2009年，加入“飞炫少年”组合，两年后退出。2013年1月，发行个人首支单曲《梦想摩天楼》；同年8月6日，易烊千玺与王俊凯、王源以TFBOYS组合形式出道。",
+     pic1:"/images/one.jpg",
+     zhuan:"/images/zhuan.png",
+     xiao:"/images/liaotian.png",
+     zan:"/images/zan.png",
+     zhuanfa:"转发",
+     xiaoxi:"1",
+     dianzan:"赞"
+
+   },
+   {
+    pic: "/images/touxiang1.png",
+     name:"化妆种草妹",
+     small: "17分钟前",
+     word:" 2005年，易烊千玺首登电视荧屏，开始参演各类综艺节目。2009年，加入“飞炫少年”组合，两年后退出。2013年1月，发行个人首支单曲《梦想摩天楼》；同年8月6日，易烊千玺与王俊凯、王源以TFBOYS组合形式出道。2016年1月1日，易烊千玺获得湖南卫视《全员加速中》第一季总冠军，成为纯甄加速王。4月9日，易烊千玺荣获第16届音乐风云榜年",
+    pic1:"/images/two.jpg",
+    zhuan: "/images/zhuan.png",
+    xiao: "/images/liaotian.png",
+    zan: "/images/zan.png",
+    zhuanfa: "转发",
+     xiaoxi: "1",
+     dianzan: "赞"
+   },
+   {
+     pic: "/images/touxiang2.png",
+     name: "黑夜行",
+     small: "15分钟前",
+     word: "2009年，易烊千玺加入新小虎队“飞炫少年”组合，成为组合中的乖乖虎、健康少年[2]；7月20日，“飞炫少年”参与录制《成长在线》之“清凉计划”[4]；8月8日，北京奥运会一周年纪念日，易烊千玺亮相《成长在线》特别节目《回首奥运》，表演歌曲《北京欢迎你》和《我相信》[5]；8月16日，“飞炫少年”参与天津卫视综艺节目《综艺食八街》[6][7]；9月21日，“飞炫少年”参与河南省第十一届运动会开幕式演出[8]；11月27日，“风载我歌行”首都大学生校园歌手演唱会在中国人民大学举行，“飞炫少年”受邀担任表演嘉宾",
+      pic1: "/images/three.jpg",
+     zhuan: "/images/zhuan.png",
+     xiao: "/images/liaotian.png",
+     zan: "/images/zan.png",
+     zhuanfa: "转发",
+     xiaoxi: "1",
+     dianzan: "赞"
+
+   }
+   ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
