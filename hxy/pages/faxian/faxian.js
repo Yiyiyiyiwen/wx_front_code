ff237296f1565fecbc54304bf450bcde10e95af2
胡欣玥 <1695234793@qq.com>
2018-12-02 22:53:22 +0800

    2

--- hxy/pages/faxian/faxian.js
+++ hxy/pages/faxian/faxian.js
@@ -6,7 +6,35 @@ Page({
    */
   data: {
     inputShowed: false,
-    inputVal: ""
+    inputVal: "",
+    array:[{
+      t1:"刘丹回应杨幂..",
+      t2:"杨幂礼服",
+      t3:"吴尊回应浪费原型",
+      t4:"吐槽大会"
+    }],
+    array1:[{
+      t1:"吴承恩故居挂满六小时..",
+      t2:"感觉闺蜜就是前..",
+      t3:"成年人适不适合看天..",
+      t4:"更多热搜 >"
+    }],
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    currentData: 0,
+    array2: [{
+      pic: "/images/touxiang.png",
+      biaoti: "内容列表标题",
+      content: "文字文字文字文字文字文字文字文字文字",
+      pic1: "/images/taolun.png"
+    }]
 
   },
 
@@ -85,5 +113,25 @@ Page({
     this.setData({
       inputVal: e.detail.value
     });
+  },
+  //获取当前滑块的index
+  bindchange: function (e) {
+    const that = this;
+    that.setData({
+      currentData: e.detail.current
+    })
+  },
+  //点击切换，滑块index赋值
+  checkCurrent: function (e) {
+    const that = this;
+
+    if (that.data.currentData === e.target.dataset.current) {
+      return false;
+    } else {
+
+      that.setData({
+        currentData: e.target.dataset.current
+      })
+    }
   }
 })
